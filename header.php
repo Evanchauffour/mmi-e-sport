e2300aa3e2b91c885a401c316037971259f9a577
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-01-31 14:00:29 +0100

    initalisation et debut d'accueil

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../mmi-e-sport/assets/css/app.css">
+    <title>MMI E-SPORT</title>
+</head>
+<body>
+    <header>
+        <img src="../mmi-e-sport/assets/img/logo.svg" alt="">
+            <ul>
+                <li data-page='1'>Accueil</li>
+                <li data-page='2'>Programme</li>
+                <li data-page='3'>Jeux</li>
+                <li data-page='4'>Contact</li>
+                <li>Se connecter</li>
+            </ul>
+
+    </header>
+</body>
+</html>
