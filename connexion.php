932fa48953b2d6adc71e9f42a2641c8ba43a7d45
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-02-02 17:35:03 +0100

    coco

--- connexion.php
+++ connexion.php
@@ -7,21 +7,24 @@
 </head>
 <body>
 <div class="container-connexion">
-    <h1>Connexion</h1>
-    <form action="">
-        <div class="container-input_label">
-            <label for="email">Mail universitaire</label>
-            <input type="email" name="email">
-        </div>
-        <div class="container-input_label">
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password">
-        </div>
-        <button>Se connecter</button>
-    </form>
-    <p>Pas de compte ? <span><a href="inscription.php">Inscris-toi !</a></span></p>
+    <div class="info-connexion">
+        <h1>Connexion</h1>
+        <form action="">
+            <div class="container-input_label">
+                <label for="email">Mail universitaire</label>
+                <input type="email" name="email">
+            </div>
+            <div class="container-input_label">
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password">
+            </div>
+            <button>Se connecter</button>
+        </form>
+        <p>Pas de compte ? <span><a href="inscription.php">Inscris-toi !</a></span></p>
+        <img class="bg-connexion" src="../mmi-e-sport/assets/img/bg-connexion.svg" alt=""> 
+    </div>
+    <a href="index.php" class="back-to-home">Retour site web...</a>
 </div>
-<img class="bg-connexion" src="../mmi-e-sport/assets/img/bg-connexion.svg" alt="">
 
 <?php
     require('footer.php');
--- inscription.php
+++ inscription.php
@@ -1,32 +1,43 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../mmi-e-sport/assets/css/app.css">
+    <title>MMI E-SPORT</title>
+</head>
 <body>
-<?php
-    require('header.php')
-?>
 <div class="container-inscription">
-    <h1>Inscription</h1>
-    <form action="">
-        <div class="container-input_label">
-            <label for="pseudo">Pseudonyme</label>
-            <input type="text" name="pseudo">
-        </div>
-        <div class="container-input_label">
-            <label for="email">Mail universitaire</label>
-            <input type="email" name="email">
-        </div>
-        <div class="container-input_label">
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password">
-        </div>
-        <div class="container-input_label">
-            <label for="confirme">Confirmer Mot de passe</label>
-            <input type="password" name="confirme">
-        </div>
-        <button>Se connecter</button>
-    </form>
-    <p>Déjà un compte ? <span><a href="connexion.php">Connecte-toi !</a></span></p>
+    <div class="info-inscription">
+        <h1>Inscription</h1>
+        <form action="">
+            <div class="container-input">
+                <div class="container-input_label">
+                    <label for="pseudo">Pseudonyme</label>
+                    <input type="text" name="pseudo">
+                </div>
+                <div class="container-input_label">
+                    <label for="email">Mail universitaire</label>
+                    <input type="email" name="email">
+                </div>
+                <div class="container-input_label">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password">
+                </div>
+                <div class="container-input_label">
+                    <label for="confirmePassword">Confirmer mot de passe</label>
+                    <input type="password" name="confirmePassword">
+                </div>
+            </div>
+            <button>S'inscrire</button>
+        </form>
+        <p>Pas de compte ? <span><a href="connexion.php">Inscris-toi !</a></span></p>
+        <img class="bg-connexion" src="../mmi-e-sport/assets/img/bg-connexion.svg" alt=""> 
+    </div>
+    <a href="index.php" class="back-to-home">Retour site web...</a>
 </div>
+
 <?php
-    require('footer.php')
+    require('footer.php');
 ?>
 </body>
 </html>
